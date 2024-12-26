d19d5bcb8cff88b761a7fa1b4722ee4a0663d347
Chun-Yi Kuan <phantom042109@gmail.com>
2024-12-26 19:56:13 +0800

    Update README.md and related codes.

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Speech-Copilot: Leveraging Large Language Models for Speech Processing via Task Decomposition, Modularization, and Program Generation
 
-<p align="center">
-  <a href="https://2024.ieeeslt.org/">[SLT2024]</a> <a href="https://arxiv.org/abs/2407.09886">[arXiv]</a> <a href="https://sites.google.com/view/slt2024-demo-page">[Demo]</a>
+<p align="center" style="font-size: 16px;">
+  <a href="https://2024.ieeeslt.org/">[SLT2024]</a> 
+  <a href="https://arxiv.org/abs/2407.09886">[arXiv]</a> 
+  <a href="https://sites.google.com/view/slt2024-demo-page">[Demo]</a>
 </p>
+<div style="text-align: center;">
+  <div style="display: flex; align-items: center; justify-content: center;">
+    <img src="images/speech-copilot-logo.png" alt="logo" style="width: 150px; margin-right: 10px;">
+    <p></p>
+  </div>
+</div>
 
-The official Github page of the paper "Speech-Copilot: Leveraging Large Language Models for Speech Processing via Task Decomposition, Modularization, and Program Generation". 
+
+### The official Github page of the paper "Speech-Copilot: Leveraging Large Language Models for Speech Processing via Task Decomposition, Modularization, and Program Generation". 
 - Authors: Chun-Yi Kuan<sup>🐤</sup>, Chih-Kai Yang<sup>🐤</sup>, Wei-Ping Huang<sup>🐦 </sup>, Ke-Han Lu<sup>🐦 </sup>, Hung-yi Lee
 - 🐤 🐦: Equal Contribution 
 - Affiliation: National Taiwan University
@@ -12,10 +21,11 @@ The official Github page of the paper "Speech-Copilot: Leveraging Large Language
 - arXiv Link: https://arxiv.org/abs/2407.09886
 - Refer to our demo [website](https://sites.google.com/view/slt2024-demo-page) for more examples. 👍
 
-## Overview
-![overview](https://github.com/user-attachments/assets/337c0995-b314-4635-a2aa-d816240d04a6)
+## Overview ⭐
+<img src="images/overview.png" alt="overview" title="overview" style="width:55%;">
 
-## Abstract
+
+## Abstract 🔥
 In this work, we introduce Speech-Copilot, a modular framework for instruction-oriented speech-processing tasks that minimizes human effort in toolset construction. 
 Unlike end-to-end methods using large audio-language models, Speech-Copilot builds speech processing-specific toolsets by analyzing pre-collected task instructions and breaking tasks into manageable sub-tasks. 
 It features a flexible agent based on large language models that performs tasks through program generation. 
@@ -24,12 +34,30 @@ Key contributions include: 1) developing an innovative framework for speech proc
 Without additional training processes required by end-to-end approaches, our method provides a flexible and extendable solution for a wide range of speech-processing applications.
 
 ## Evaluation Results 🏆
-<img width="1078" alt="result" src="https://github.com/user-attachments/assets/029a1399-5318-4f7f-8873-d5aa172034dc">
+<div style="display: flex; justify-content: flex-start; align-items: center;">
+  <img src="images/radar-chart.png" alt="chart" style="width: 30%; margin-right: 10px;">
+  <img src="images/overall-performance.png" alt="overall" style="width: 30%;">
+</div>
 
-## Code
-Coming Soon.
+<img src="images/results.png" alt="results" title="results" style="width:55%;">
 
-## Baseline Models
+## Tooleset Construction 🔧
+### Step1: User Instruction
+- (1) Collect a pool of user instructions.
+### Step2: Tasks Decomposition
+- (1) Break down all the user instructions in the given pool and identify the speech modules required for each instruction.
+- (2) Refer to `task_decomposition.py` for details.
+### Step3: Tasks Modularization
+- (1) After identifying the necessary speech modules, users can choose their preferred speech modules to serve as the backbone for these tasks.
+- (2) Examples are provided in `speech_module_generated_by_LLM.py`.
+### Step4: Program Generation
+- (1) Use an LLM to analyze the given user instruction and generate a solution program based on the speech modules created in the previous steps.
+- (2) Refer to `program_generation.py` for guidance.
+- (3) In `program_generation.py`, we provide an example task from [Dynamic-SUPERB](https://github.com/dynamic-superb/dynamic-superb) called [EmotionRecognition_MultimodalEmotionlinesDataset](https://huggingface.co/datasets/DynamicSuperb/EmotionRecognition_MultimodalEmotionlinesDataset) with the instruction: `Recognize and organize the emotional expressions in the spoken words. The answer could be anger, disgust, sadness, joy, neutral, surprise, or fear.`
+### Step5: Program Execution
+- (1) Execute the program generated in Step 4. Ensure the required speech modules from Step 3 are properly set up beforehand, so they can be utilized during execution.
+
+## Baseline Models 🌲
 - Qwen-Audio-Chat
     - Qwen-audio: Advancing universal audio understanding via unified large-scale audio-language models [[arXiv](https://arxiv.org/abs/2311.07919)]
 
@@ -46,3 +74,4 @@ Coming Soon.
     - ASR: Robust Speech Recognition via Large-Scale Weak Supervision [[arXiv](https://arxiv.org/abs/2212.04356)]
     - Audio Captioning Model: Qwen-audio: Advancing universal audio understanding via unified large-scale audio-language models [[arXiv](https://arxiv.org/abs/2311.07919)]
     - LLM: gpt-3.5-turbo
+
---
+++ task_decomposition.py
@@ -0,0 +1,121 @@
+import os
+import re
+import json
+from openai import OpenAI
+from tqdm import tqdm
+import argparse
+
+
+def query_pivotal_llm_for_multi_turn(client, messages, model_name="gpt-3.5-turbo", temperature=0.0, top_p=1.0, max_tokens=256, seed=1126):
+
+    chat_completion = client.chat.completions.create(
+        messages=messages,
+        model=model_name,
+        temperature=temperature,
+        top_p=top_p,
+        max_tokens=max_tokens,
+        seed=seed
+    )
+
+    return chat_completion.choices[0].message.content
+
+
+def task_decomposition_prompt_template():
+
+    system_prompt = "You are an expert on speech domain and good at coding."
+
+    user_prompt_1 = f'''
+You are given the following task instructions, where each instruction may require you to do a speech processing task.
+While the required tasks are diverse, they may have some common sub-tasks.
+For examples, automatic speech recognition may be helpful for several tasks.
+Another example is that some tasks are sound classification tasks, and these tasks may be solved using a single module.
+Now, I want you to analyze the instructions, identify the corresponding speech processing tasks, and solve the tasks in the instructions by writing the Python code for each task.
+In your code, you should modularize the sub-tasks as a code module, e.g. speech_recognition(audio), query_LLM(prompt), etc.
+Then you should finish the tasks by calling the code modules of sub-tasks. 
+However, as you don't have the official module list, you can create modules when you need. The constraint is that you should reuse the modules as much as possible. And make the size of th module list as small as possible.
+This means that if two different tasks require the same sub-task, you should use the same module to finish the sub-task instead of creating a similar one again.
+Please keep in mind that you don't need to implement the modules, just define them with a placeholder like speech_recognition(audio), and we will complete the implementation for you.
+Besides, here is a module named query_LLM(prompt) that can be used to query the language model for extracting information with proper prompt. You should make use of this module and reduce the number of modules you create.
+If the task can be correctly solved by using automatic speech recognition and query LLM, you should use these two modules to finish the task instead of creating a new one.
+However, the query_llm module is not omnipotent. It cannot have the audio information, and the speech recognition will only transcribe the speech into text, and some audio information like noise level, or speaker information will be lost during the transcription.
+Hence, if the task require audio information beyond the transcription, you should use other modules.
+
+Here is an example:
+Suppose you are given the following instruction: Give me the transcription of the following audio file.
+This will require you to do automatic speech recognition. You can write a module called speech_recognition(audio) to finish this task.
+The final output should have the format like this:
+`````python`````
+# Code for instruction 1
+# Task needed: automatic speech recognition
+def execute_command(audio):
+    transcription = speech_recognition(audio)
+    return transcription
+
+# Code for instruction 2
+# Task needed: sound classification
+def execute_command(audio):
+    sound_class = sound_classification(audio)
+    return sound_class
+
+# Code for instruction 3
+# Task needed: SNR estimation
+def execute_command(audio):
+    SNR = SNR_estimation(audio)
+    if SNR > 15:
+        return "clean"
+    else:
+        return "noisy"
+````````````````
+If multiple instructions are provided, you should write the code for each instruction separately and index them in the final output.
+Now given the following task instruction, please write the Python code to solve the tasks. The output should follow the format above.
+Instruction: 
+'''
+    return system_prompt, user_prompt_1
+
+# Part1: Task Decomposition.
+# Provide your own task instructions in the below list.
+instruction_list = [...]
+
+system_prompt, user_prompt_1 = task_decomposition_prompt_template()
+
+for i in range(len(instruction_list)):
+    user_prompt_1 += f"Instruction {i+1}: {instruction_list[i]}\n"
+
+print(user_prompt_1)
+
+messages = [
+    {"role": "system", "content": system_prompt},
+    {"role": "user", "content": user_prompt_1}
+]
+
+openai_api_key = "YOUR_OPENAI_API_KEY"
+client = OpenAI(api_key=openai_api_key)
+
+results = query_pivotal_llm_for_multi_turn(client, messages, model_name="gpt-4o", temperature=0.0, top_p=1.0, max_tokens=4096)
+print(results)
+with open('task_decomposition.txt', 'w') as f:
+    f.write(results)
+
+
+# Part2: Deduplicate the used modules.
+prompt_for_summarize = f'''
+Given the following code:
+{results}
+
+Please count and summarize the used modules for me. The format should be:
+1. speech_recognition: the number of usage of speech_recognition
+2. query_LLM: the number of usage of query_LLM
+...
+
+Don't generate code. Just summarize the used modules.
+'''
+
+
+message_for_summarize= [
+    {"role": "user", "content": prompt_for_summarize}
+]
+
+summarize_for_first_correction = query_pivotal_llm_for_multi_turn(client, message_for_summarize, model_name="gpt-4o", temperature=0.0, top_p=1.0, max_tokens=4096)
+print(summarize_for_first_correction)
+with open('deduplicate_task_decomposition.txt', 'w') as f:
+    f.write(summarize_for_first_correction)
